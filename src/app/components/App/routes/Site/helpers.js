471d4526f5aabffeef1d6a2989af865214cd0372
William Oliveira <william.oliveir4@gmail.com>
2018-04-23 04:21:36 -0300

    fix cdn

--- src/app/components/App/routes/Site/helpers.js
+++ src/app/components/App/routes/Site/helpers.js
@@ -1,4 +1,4 @@
-import config from '../../../../../../config'
+import config from '../../../../../config'
 import defaultProductImg from '../../../../imgs/default_product.jpg'
 
 export const formatCurrency = (price = 0, prefix = 'R$ ') =>
@@ -6,9 +6,16 @@ export const formatCurrency = (price = 0, prefix = 'R$ ') =>
     .toFixed(2)
     .replace('.', ',')}`
 
-export const imgCdn = url => `//i0.wp.com/${url.replace(/https?:\/\//, '')}`
+export const imgCdn = (url) => {
+  if (process.env.BUILD_FLAG_IS_DEV === 'true') return url
+  return `//i0.wp.com/${url.replace(/https?:\/\//, '')}`
+}
 
-export const makeImageUrl = url => imgCdn(`${config('mediaUrl')}${url}`)
+export const makeImageUrl = (url) => {
+  const urlWithHttp = url.startsWith('http') ? url : `${config('mediaUrl')}${url}`
+
+  return imgCdn(urlWithHttp)
+}
 
 export const getProductThumbUrl = product =>
   (product.images && product.images.length > 0
